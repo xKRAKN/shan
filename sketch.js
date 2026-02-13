56532ce5559bea655baa5f670c0de669dffafb39
Gilbert Ken Colegado <163809623+xKRAKN@users.noreply.github.com>
2026-02-13 11:33:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1); // Forces GitHub to render exactly like p5.js
+  angleMode(DEGREES);
+  initGarden();
+}
 let stemHeight = 0;
 let maxStemHeight;
 let bloomScale = 0;
